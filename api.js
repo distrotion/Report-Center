91bc278a3f6e1c98b58507d36a752f2c0b46f02a
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2023-03-10 08:02:57 +0700

    .

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use(require("./flow/001/INS_REPORT"))
+router.use(require("./flow/001/GW_REPORT"))
 router.use(require("./flow/testflow/testflow"))
 
 module.exports = router;

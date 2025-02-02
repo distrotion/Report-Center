b48b79a2e7ae96c7f1fb5e9fde498392a2a1d577
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-02-02 17:04:03 +0700

    .

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ router.use(require("./flow/001/INS_REPORT"))
 router.use(require("./flow/001/GW_REPORT"))
 router.use(require("./flow/001/HES_G_REPORT"))
 router.use(require("./flow/001/BP12_PH_REPORT"))
+router.use(require("./flow/001/HES_PH_REPORT"))
 router.use(require("./flow/001/PICPDF"))
 
 router.use(require("./flow/testflow/testflow"))

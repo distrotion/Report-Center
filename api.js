70c4623ab263877a18da6614d045e744487c5ca7
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-04-18 20:19:23 +0700

    .

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+//BP12_G_REPORT
+
 router.use(require("./flow/001/INS_REPORT"))
 router.use(require("./flow/001/GW_REPORT"))
 router.use(require("./flow/001/HES_G_REPORT"))
+router.use(require("./flow/001/BP12_G_REPORT"))
 router.use(require("./flow/001/BP12_PH_REPORT"))
 router.use(require("./flow/001/HES_PH_REPORT"))
 router.use(require("./flow/001/PICPDF"))

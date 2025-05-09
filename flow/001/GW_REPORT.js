2907a7b29ec333637ccb4bb9466b6f7c42b29566
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-05-09 14:55:24 +0700

    .

--- flow/001/GW_REPORT.js
+++ flow/001/GW_REPORT.js
@@ -23,6 +23,7 @@ let LOAD = "LOAD";
 let CORETYPE = "CORETYPE";
 let FREQUENCY = "FREQUENCY";
 let PATTERN_01 = "PATTERN_01";
+let DESIMAL = "DESIMAL";
 
 
 router.get('/FINALMASTER', async (req, res) => {
@@ -42,6 +43,7 @@ router.post('/GW_Report_PDF', async (req, res) => {
   let find7 = [];
   let find8 = [];
   let find9 = [];
+  let find10= [];
 
   let DATA = [];
   let PATTERNs = [];
@@ -57,6 +59,7 @@ router.post('/GW_Report_PDF', async (req, res) => {
     find7 = await mongodb.find(masterDB, CALCULATE, { "activeid": "active_id" });
     find8 = await mongodb.find(masterDB, SPECIFICATION, { "activeid": "active_id" });
     find9 = await mongodb.find(masterDB, UNIT, { "activeid": "active_id" });
+    find10 = await mongodb.find(masterDB, DESIMAL, { "activeid": "active_id" });
 
 
     DATA = await mongodb.find("MAIN_DATA", "MAIN", { "PO": `${input['PO']}` });
@@ -67,8 +70,53 @@ router.post('/GW_Report_PDF', async (req, res) => {
 
 
 
-  return res.json({ "DATA": DATA, "PATTERN": PATTERNs, "TYPE": find1, "ITEMS": find2, "METHOD": find3, "RESULTFORMAT": find4, "GRAPHTYPE": find5, "INSTRUMENTS": find6, "CALCULATE": find7 , "SPECIFICATION": find8 , "UNIT": find9 });
+  return res.json({ "DATA": DATA, "PATTERN": PATTERNs, "TYPE": find1, "ITEMS": find2, "METHOD": find3, "RESULTFORMAT": find4, "GRAPHTYPE": find5, "INSTRUMENTS": find6, "CALCULATE": find7 , "SPECIFICATION": find8 , "UNIT": find9, "DESIMAL": find10 });
 });
+
+router.get('/FINALMASTER', async (req, res) => {
+  return res.json("READY");
+});
+
+// router.post('/GW_Report_PDF', async (req, res) => {
+//   //-------------------------------------
+//   console.log("--GW_Report_PDF--");
+//   let input = req.body;
+//   let find1 = [];
+//   let find2 = [];
+//   let find3 = [];
+//   let find4 = [];
+//   let find5 = [];
+//   let find6 = [];
+//   let find7 = [];
+//   let find8 = [];
+//   let find9 = [];
+
+//   let DATA = [];
+//   let PATTERNs = [];
+//   //-------------------------------------
+//   if (input['PO'] != undefined) {
+
+//     find1 = await mongodb.find(masterDB, TYPE, { "activeid": "active_id" });
+//     find2 = await mongodb.find(masterDB, ITEMs, { "activeid": "active_id" });
+//     find3 = await mongodb.find(masterDB, MACHINE, { "activeid": "active_id" });
+//     find4 = await mongodb.find(masterDB, RESULTFORMAT, {});
+//     find5 = await mongodb.find(masterDB, GRAPHTYPE, {});
+//     find6 = await mongodb.find(masterDB, INSTRUMENTS, {});
+//     find7 = await mongodb.find(masterDB, CALCULATE, { "activeid": "active_id" });
+//     find8 = await mongodb.find(masterDB, SPECIFICATION, { "activeid": "active_id" });
+//     find9 = await mongodb.find(masterDB, UNIT, { "activeid": "active_id" });
+
+
+//     DATA = await mongodb.find("MAIN_DATA", "MAIN", { "PO": `${input['PO']}` });
+//     if (DATA.length > 0) {
+//       PATTERNs = await mongodb.find(PATTERN, PATTERN_01, { "CP": `${DATA[0]['MATCP']}` });
+//     }
+//   }
+
+
+
+//   return res.json({ "DATA": DATA, "PATTERN": PATTERNs, "TYPE": find1, "ITEMS": find2, "METHOD": find3, "RESULTFORMAT": find4, "GRAPHTYPE": find5, "INSTRUMENTS": find6, "CALCULATE": find7 , "SPECIFICATION": find8 , "UNIT": find9 });
+// });
 
 router.get('/FINALMASTER', async (req, res) => {
   return res.json("READY");

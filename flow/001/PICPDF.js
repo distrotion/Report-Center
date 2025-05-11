74038380b37596a02e656ee26a16d8c9847c8cdc
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-05-11 11:43:06 +0700

    .

--- flow/001/PICPDF.js
+++ flow/001/PICPDF.js
@@ -8,9 +8,9 @@ const { jsPDF } = require("jspdf");
 const pdf2base64 = require('pdf-to-base64');
 var fs = require('fs');
 
-Number.prototype.pad = function(n) {
-  if (n===undefined)
-      n = 2;
+Number.prototype.pad = function (n) {
+  if (n === undefined)
+    n = 2;
 
   return (new Array(n).join('0') + this).slice(-n);
 }
@@ -21,13 +21,13 @@ router.post('/goPDF', async (req, res) => {
   //-------------------------------------
   console.log("--goPDF--");
   let input = req.body;
- 
+
 
   // console.log(input[`PIC`])
   let bitmap = ``
 
-  if (input[`PIC`] != undefined,input[`PO`] != undefined) {
-    let doc = new jsPDF("p", "mm", "a4",true);
+  if (input[`PIC`] != undefined, input[`PO`] != undefined) {
+    let doc = new jsPDF("p", "mm", "a4", true);
     var width = doc.internal.pageSize.getWidth();
     var height = doc.internal.pageSize.getHeight();
 
@@ -39,37 +39,96 @@ router.post('/goPDF', async (req, res) => {
     // let day = d.getDate();
     let month = d.getMonth();
     let year = d.getFullYear();
+    //
     console.log(month.pad(2))
     console.log(year.pad(4))
-  
+
     // var dir = `\\\\172.20.10.150\\sap_s4hana\\S4PRD\\HSORDERSHEET_PP\\${year.pad(4)}${(month+1).pad(2)}\\${input['PO']}`;
     var dir = `\\\\172.20.10.150\\sap_s4hana\\S4PRD\\HSORDERSHEET_PP\\Input\\${input['PO']}`;
- 
 
     //S4PRD\HSORDERSHEET_PP\Input
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
-  
+
 
     //save file แล้วผม read มาเป็น base64 ส่งกลับ
-   
-  await pdf2base64(`${input[`PO`]}.pdf`)
-    .then(
+
+    await pdf2base64(`${input[`PO`]}.pdf`)
+      .then(
         (response) => {
-            // console.log(response); 
-            fs.rmSync(`${input[`PO`]}.pdf`);
-            bitmap = response;
-           
+          // console.log(response); 
+          fs.rmSync(`${input[`PO`]}.pdf`);
+          bitmap = response;
+
         }
-    )
-    .catch(
+      )
+      .catch(
         (error) => {
           fs.rmSync(`${input[`PO`]}.pdf`);
           // console.log(error); //Exepection error....
 
         }
-    )
+      )
+
+  }
+
+  return res.json({ "PIC": bitmap });
+});
+
+router.post('/genfloder', async (req, res) => {
+  //-------------------------------------
+  console.log("--genfloder--");
+  let input = req.body;
+
+
+  // console.log(input[`PIC`])
+  let bitmap = ``
+
+  if (input[`PIC`] != undefined, input[`PO`] != undefined) {
+    // let doc = new jsPDF("p", "mm", "a4", true);
+    // var width = doc.internal.pageSize.getWidth();
+    // var height = doc.internal.pageSize.getHeight();
+
+    // doc.addImage(`data:image/jpeg;base64,` + input[`PIC`], 'JPEG', 0, 0, width, height)
+
+    // doc.save(`${input[`PO`]}.pdf`)
+
+    // const d = new Date();
+    // // let day = d.getDate();
+    // let month = d.getMonth();
+    // let year = d.getFullYear();
+    // //
+    // console.log(month.pad(2))
+    // console.log(year.pad(4))
+
+    // var dir = `\\\\172.20.10.150\\sap_s4hana\\S4PRD\\HSORDERSHEET_PP\\${year.pad(4)}${(month+1).pad(2)}\\${input['PO']}`;
+    var dir = `\\\\172.20.10.150\\sap_s4hana\\S4PRD\\HSORDERSHEET_PP\\Input\\${input['PO']}`;
+
+    //S4PRD\HSORDERSHEET_PP\Input
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+
+    //save file แล้วผม read มาเป็น base64 ส่งกลับ
+
+    // await pdf2base64(`${input[`PO`]}.pdf`)
+    //   .then(
+    //     (response) => {
+    //       // console.log(response); 
+    //       fs.rmSync(`${input[`PO`]}.pdf`);
+    //       bitmap = response;
+
+    //     }
+    //   )
+    //   .catch(
+    //     (error) => {
+    //       fs.rmSync(`${input[`PO`]}.pdf`);
+    //       // console.log(error); //Exepection error....
+
+    //     }
+    //   )
 
   }
 
@@ -80,13 +139,13 @@ router.post('/goPDF_TESTSAP', async (req, res) => {
   //-------------------------------------
   console.log("--goPDF--");
   let input = req.body;
- 
+
 
   // console.log(input[`PIC`])
   let bitmap = ``
 
-  if (input[`PIC`] != undefined,input[`PO`] != undefined) {
-    let doc = new jsPDF("p", "mm", "a4",true);
+  if (input[`PIC`] != undefined, input[`PO`] != undefined) {
+    let doc = new jsPDF("p", "mm", "a4", true);
     var width = doc.internal.pageSize.getWidth();
     var height = doc.internal.pageSize.getHeight();
 
@@ -100,31 +159,31 @@ router.post('/goPDF_TESTSAP', async (req, res) => {
     let year = d.getFullYear();
     console.log(month.pad(2))
     console.log(year.pad(4))
-  
+
     var dir = `\\\\172.20.10.150\\sap_s4hana\\S4DEV\\HSORDERSHEET_PP\\input\\${input['PO']}`;
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
-  
+
 
     //save file แล้วผม read มาเป็น base64 ส่งกลับ
-   
-  await pdf2base64(`${input[`PO`]}.pdf`)
-    .then(
+
+    await pdf2base64(`${input[`PO`]}.pdf`)
+      .then(
         (response) => {
-            // console.log(response); 
-            fs.rmSync(`${input[`PO`]}.pdf`);
-            bitmap = response;
-           
+          // console.log(response); 
+          fs.rmSync(`${input[`PO`]}.pdf`);
+          bitmap = response;
+
         }
-    )
-    .catch(
+      )
+      .catch(
         (error) => {
           fs.rmSync(`${input[`PO`]}.pdf`);
           // console.log(error); //Exepection error....
 
         }
-    )
+      )
 
   }
 
@@ -148,9 +207,9 @@ router.post('/RAWDATA/sapget', async (req, res) => {
         "TABLE_NAME": "PPORDER"
       });
       // if (resp.status == 200) {
-        let returnDATA = resp;
-        output = returnDATA["Records"] || []
-        //  console.log(output)
+      let returnDATA = resp;
+      output = returnDATA["Records"] || []
+      //  console.log(output)
       // }
     } catch (err) {
       output = [];
